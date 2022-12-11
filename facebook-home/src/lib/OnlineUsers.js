2803e394391fafa071621edc4d56cbbbaa4c4467
Manoj Barman <the_manoj@live.com>
2022-12-11 12:04:13 +0530

    changed sachin's photo url

--- facebook-home/src/lib/OnlineUsers.js
+++ facebook-home/src/lib/OnlineUsers.js
@@ -1,97 +1,97 @@
 export const OnlineUsers = [
   {
     id: 1,
-    name: 'Ratan Tata',
+    name: "Ratan Tata",
     avatar:
-      'https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco,dpr_1/v1397188430/e49b47a162b582593855d3a2828e0ba5.jpg',
+      "https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco,dpr_1/v1397188430/e49b47a162b582593855d3a2828e0ba5.jpg",
     hasStory: false,
   },
   {
     id: 2,
-    name: 'Sachin Tendulkar',
+    name: "Sachin Tendulkar",
     avatar:
-      'https://pbs.twimg.com/profile_images/1410819014255730689/u76ZqFWN_400x400.jpg',
+      "https://m.cricbuzz.com/a/img/v1/192x192/i1/c171004/sachin-tendulkar.jpg",
     hasStory: false,
   },
   {
     id: 3,
-    name: 'Sundar Pichai',
+    name: "Sundar Pichai",
     avatar:
-      'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/bjrd7lfsay6cao9jzkmr',
+      "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/bjrd7lfsay6cao9jzkmr",
     hasStory: true,
   },
   {
     id: 4,
-    name: 'Mark Zuckerberg',
+    name: "Mark Zuckerberg",
     avatar:
-      'https://pbs.twimg.com/profile_images/730271326150103042/gR9RVbmC_400x400.jpg',
+      "https://pbs.twimg.com/profile_images/730271326150103042/gR9RVbmC_400x400.jpg",
     hasStory: false,
   },
   {
     id: 5,
-    name: 'Jony Ive',
-    avatar: 'https://pbs.twimg.com/media/BJQuj0OCAAAzcAB.jpg',
+    name: "Jony Ive",
+    avatar: "https://pbs.twimg.com/media/BJQuj0OCAAAzcAB.jpg",
     hasStory: false,
   },
   {
     id: 6,
-    name: 'Tim Cook',
+    name: "Tim Cook",
     avatar:
-      'https://pbs.twimg.com/profile_images/1535420431766671360/Pwq-1eJc_400x400.jpg',
+      "https://pbs.twimg.com/profile_images/1535420431766671360/Pwq-1eJc_400x400.jpg",
     hasStory: false,
   },
   {
     id: 7,
-    name: 'Robert Downey Jr.',
+    name: "Robert Downey Jr.",
     avatar:
-      'https://pbs.twimg.com/profile_images/685915759055351808/ILeBa4II_400x400.png',
+      "https://pbs.twimg.com/profile_images/685915759055351808/ILeBa4II_400x400.png",
     hasStory: false,
   },
   {
     id: 8,
-    name: 'APJ Abdul Kalam',
+    name: "APJ Abdul Kalam",
     avatar:
-      'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/94/43/3a/94433adf-92a9-1a65-1b71-338fdfe0b238/source/256x256bb.jpg',
+      "https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/94/43/3a/94433adf-92a9-1a65-1b71-338fdfe0b238/source/256x256bb.jpg",
     hasStory: false,
   },
   {
     id: 9,
-    name: 'Tony Vonachen',
-    avatar: 'https://xsgames.co/randomusers/assets/avatars/male/23.jpg',
+    name: "Tony Vonachen",
+    avatar: "https://xsgames.co/randomusers/assets/avatars/male/23.jpg",
     hasStory: false,
   },
   {
     id: 10,
-    name: 'Irrfan Khan',
+    name: "Irrfan Khan",
     avatar:
-      'https://i.tribune.com.pk/media/images/1737453-irrfankhan-1529396073/1737453-irrfankhan-1529396073-400x230.jpg',
+      "https://i.tribune.com.pk/media/images/1737453-irrfankhan-1529396073/1737453-irrfankhan-1529396073-400x230.jpg",
     hasStory: false,
   },
   {
     id: 11,
-    name: 'MS Dhoni',
+    name: "MS Dhoni",
     avatar:
-      'https://is4-ssl.mzstatic.com/image/thumb/Purple125/v4/26/63/a9/2663a9aa-7f93-2e7c-00d6-c03213b79b70/source/256x256bb.jpg',
+      "https://is4-ssl.mzstatic.com/image/thumb/Purple125/v4/26/63/a9/2663a9aa-7f93-2e7c-00d6-c03213b79b70/source/256x256bb.jpg",
     hasStory: true,
   },
   {
     id: 12,
-    name: 'Virat Kohli',
+    name: "Virat Kohli",
     avatar:
-      'https://i.pinimg.com/originals/2a/50/fa/2a50fa473599173a78135bb6f1f94a2b.jpg',
+      "https://i.pinimg.com/originals/2a/50/fa/2a50fa473599173a78135bb6f1f94a2b.jpg",
     hasStory: false,
   },
   {
     id: 13,
-    name: 'Emma Watson',
+    name: "Emma Watson",
     avatar:
-      'https://pbs.twimg.com/profile_images/707640504306016256/YcoKdF59_400x400.jpg',
+      "https://pbs.twimg.com/profile_images/707640504306016256/YcoKdF59_400x400.jpg",
     hasStory: true,
   },
   {
     id: 14,
-    name: 'Millie Bobby Brown',
-    avatar: 'https://a.wattpad.com/useravatar/ElevenlikesEGGOS.256.519393.jpg',
+    name: "Millie Bobby Brown",
+    avatar: "https://a.wattpad.com/useravatar/ElevenlikesEGGOS.256.519393.jpg",
     hasStory: false,
   },
 ];
